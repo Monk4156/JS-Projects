8366b80dc5fda8306ec788efcef041689add3e57
Monk4156 <mayank415619@gmail.com>
2023-06-16 21:24:50 +0530

    Minor changes

--- Name Changer/NC.js
+++ Name Changer/NC.js
@@ -1,7 +1,5 @@
 
 function ChangeName() {
 var changeName = document.getElementById("inputtext");
-if (changeName.value != "") {
-changeName.value = "I'm Stupid";
-}
+changeName.value = "I'm Stupid!";
 }
